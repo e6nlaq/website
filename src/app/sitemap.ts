7c4890f66183e026f736b8ccf874835ff3052d50
e6nlaq <120777733+e6nlaq@users.noreply.github.com>
2025-12-07 23:36:35 +0900

    fix: exclude page

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,9 +2,15 @@ import type { MetadataRoute } from "next";
 import { toolMeta } from "@/text/meta";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-    const urls = Object.keys(toolMeta).map((key) => ({
-        url: `https://e6nlaq.vercel.app/${key}`,
-        lastModified: new Date().toISOString(),
-    }));
+    const exclude: string[] = [
+        "ajl-binoculars",
+        "wind",
+    ] satisfies (keyof typeof toolMeta)[];
+    const urls = Object.keys(toolMeta)
+        .filter((key) => !exclude.includes(key))
+        .map((key) => ({
+            url: `https://e6nlaq.vercel.app/${key}`,
+            lastModified: new Date().toISOString(),
+        }));
     return urls;
 }
