745dfe036e02c5591e35bb8c8b1b841614cd99c7
e6nlaq <120777733+e6nlaq@users.noreply.github.com>
2025-12-12 23:13:09 +0900

    chore(deps): remove next-sitemap

--- next-sitemap.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-const config = {
-	siteUrl: "https://e6nlaq.vercel.app",
-	generateRobotsTxt: true,
-	generateIndexSitemap: false,
-	exclude: ["/wind", "/ajl-*"],
-};
-module.exports = config;
