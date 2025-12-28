0acc866791475dff5c60981691f06ecbb4f3337f
e6nlaq <120777733+e6nlaq@users.noreply.github.com>
2025-12-28 22:55:05 +0900

    style: simple 404

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,14 +3,14 @@ import { Button } from "@/components/ui/button";
 
 export default function NotFound() {
     return (
-        <div className="flex flex-col items-center *:my-8">
-            <h1 className="text-6xl font-black inline-block text-transparent bg-clip-text bg-linear-to-br from-sky-400 from-0% via-fuchsia-500 via-100%">
-                404
-            </h1>
-            <p>Not Foundってやつ</p>
-            <Button>
-                <Link href="/">Home</Link>
-            </Button>
+        <div className="flex flex-col gap-4 justify-center items-center">
+            <h1 className="text-9xl font-bold">404</h1>
+
+            <div className="flex gap-4">
+                <Button asChild variant="outline">
+                    <Link href="/">Home</Link>
+                </Button>
+            </div>
         </div>
     );
 }
