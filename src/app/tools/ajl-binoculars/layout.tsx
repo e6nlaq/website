c8c5134c7f2ff58a7c0429549db0d8805cee0d58
e6nlaq <120777733+e6nlaq@users.noreply.github.com>
2025-12-30 12:37:59 +0900

    fix: ajl-binoculars path

--- src/app/tools/ajl-binoculars/layout.tsx
+++ src/app/tools/ajl-binoculars/layout.tsx
@@ -2,5 +2,5 @@ import NoneStyle from "@/lib/layout";
 import { convertMetadata } from "@/lib/metadata";
 import { toolMeta } from "@/text/meta";
 
-export const metadata = convertMetadata(toolMeta["ajl-binoculars"]);
+export const metadata = convertMetadata(toolMeta["tools/ajl-binoculars"]);
 export default NoneStyle;
