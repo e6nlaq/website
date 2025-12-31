f1706898d303637334240085222bd0a470f8b1b7
e6nlaq <120777733+e6nlaq@users.noreply.github.com>
2025-12-31 12:24:36 +0900

    feat: add some messages

--- src/app/_index/greet.tsx
+++ src/app/_index/greet.tsx
@@ -9,33 +9,45 @@ export default function Greet() {
     useEffect(() => {
         const date = new Date();
         const hour = date.getHours();
+        const month = date.getMonth() + 1;
+        const day = date.getDate();
         let timeSpecificWords: string[];
 
-        if (date.getMonth() === 1 && date.getDate() === 1) {
+        if (month === 1 && day === 1) {
             timeSpecificWords = [
                 "Happy New Year!",
                 "あけましておめでとうございます!",
-                "新年快乐!",
+                "新年快樂!",
                 "새해 복 많이 받으세요!",
-                "สวัสดีปีใหม่!",
             ];
-        } else if (hour >= 5 && hour < 12) {
+        } else if (month === 12 && day === 31 && hour >= 21) {
+            timeSpecificWords = [
+                "See you next year",
+                "また来年",
+                "明年见",
+                "내년에 만나요",
+            ];
+        } else if (month === 12 && day === 25) {
+            timeSpecificWords = [
+                "Merry Christmas!",
+                "メリークリスマス!",
+                "聖誕快乐!",
+                "메리 크리스마스!",
+            ];
+        } else if (month === 10 && day === 31) {
+            timeSpecificWords = [
+                "Happy Halloween!",
+                "ハッピーハロウィン!",
+                "萬聖節快樂!",
+                "해피 할로윈!",
+            ];
+        } else if (hour >= 5 && hour < 10) {
             // Morning
             timeSpecificWords = [
                 "Good Morning",
                 "おはようございます",
                 "早上好",
                 "좋은 아침",
-                "สวัสดีตอนเช้า",
-            ];
-        } else if (hour >= 12 && hour < 18) {
-            // Afternoon / Day
-            timeSpecificWords = [
-                "Hello",
-                "こんにちは",
-                "你好",
-                "안녕하세요",
-                "สวัสดี",
             ];
         } else {
             // Evening / Night
@@ -44,7 +56,6 @@ export default function Greet() {
                 "こんばんは",
                 "晚上好",
                 "안녕하세요",
-                "สวัสดีตอนเย็น",
             ];
         }
         setWords(timeSpecificWords);
